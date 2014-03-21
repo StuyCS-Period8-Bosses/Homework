c898d6119e470feb8248b3be0250054006848f38
Ben <brothman7000@gmail.com>
2014-03-20 20:38:05 -0400

    Wanting you is everything

---
+++ List/LLNode.java
@@ -0,0 +1,21 @@
+public class LLNode {
+
+    private String value;
+    private LLNode linkage;
+
+    public LLNode() {
+
+	value = "";
+	linkage = null;
+    }
+
+    public LLNode (int i, LLNode node) {
+
+	value = i;
+	linkage = node; 
+
+}
+
+
+
+}
---
+++ List/Lister.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class LList implements List {
+
+}
+
+
+
+
+
+
+public class Lister {
+
+    public static void main (String[] args) {
+
+	//Hi
+    }
+}
